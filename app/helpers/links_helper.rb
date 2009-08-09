1d0885ff6844922864cf754aebd96862b5ff0037
Greg Belote <etoleb@gmail.com>
2009-08-09 21:35:42 +0000

    improved style and added logo

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -3,5 +3,9 @@ module LinksHelper
         anchor_text = url.gsub /^http:\/\/([^\/]+)/, 'http://<span class="domain">\1</span>'
         link_to( anchor_text, url, { :class => "url", :rel => "nofollow" }.merge(options) )
     end
+
+    def format_transcript( text, options={} )
+        return text.gsub /(\r?\n){2}/, '<p>'
+    end
 end
 

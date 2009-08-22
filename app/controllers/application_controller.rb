8d705e08ee7e9221da71dd7763f85016e1971bb4
Greg Belote <etoleb@gmail.com>
2009-08-22 06:26:47 +0000

    Removed unused link controller actions, added google analytics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,9 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+  protected
+
   def suppress_production
-    puts "RAILS_ENV = #{RAILS_ENV}"
     if RAILS_ENV == "production"
       render :file => "#{RAILS_ROOT}/public/404.html",  :status => 404
       false
